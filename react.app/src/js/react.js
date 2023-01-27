afb8a555aafec8cad7d787fb9328bccbdfc0d02b
Farid-Abishov <abisovfarid14@gmail.com>
2023-01-27 23:57:45 +0400

    add

--- react.app/src/js/react.js
+++ react.app/src/js/react.js
@@ -1,3 +1,20 @@
-var a;
-var b;
-console.log(4+3);
+const Normal_mode=document.querySelector(".bi-brightness-high");
+
+const Dark_mode=document.querySelector(".bi-moon");
+
+Dark_mode.addEventListener("click",()=> {
+console.log("ok");
+document.querySelector("body").style.backgroundColor = "white";
+document.querySelector("body").style.Color = "black";
+Normal_mode.style.display="inline-block";
+Normal_mode.style.color="black";
+Dark_mode.style.display="none";
+}
+);
+
+Normal_mode.addEventListener("click",()=>{
+document.querySelector("body").style.background="black"
+Normal_mode.style.display="none";
+Dark_mode.style.display="inline-block"
+
+});
