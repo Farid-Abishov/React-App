0ed7c53ee7b15520bc542c40f275812a8d4c75d4
Farid-Abishov <abisovfarid14@gmail.com>
2023-02-27 15:25:12 +0400

    responsive changes

--- react.app/src/js/react.js
+++ react.app/src/js/react.js
@@ -1,23 +1,21 @@
+const Normal_mode = document.querySelector(".bi-brightness-high");
 
-const Normal_mode=document.querySelector(".bi-brightness-high");
+const Dark_mode = document.querySelector(".bi-moon");
 
-const Dark_mode=document.querySelector(".bi-moon");
+const cntn_container = document.querySelector(".cntnt_container");
 
-const cntn_container=document.querySelector(".cntnt_container")
+Dark_mode.addEventListener("click", () => {
+  document.body.classList.add("dark");
+  Normal_mode.style.display = "inline-block";
+  Dark_mode.style.display = "none";
+  Normal_mode.style.color = "black";
+  cntn_container.style.backgroundColor = "white";
+});
 
-Dark_mode.addEventListener("click",()=> {
-console.log("ok");
-document.querySelector("body").style.backgroundColor = "white";
-Normal_mode.style.display="inline-block";
-Dark_mode.style.display="none";
-Normal_mode.style.color="black";
-cntn_container.style.backgroundColor="white"
-}
-);
-
-Normal_mode.addEventListener("click",()=>{
-document.querySelector("body").style.backgroundColor="#111828"
-Normal_mode.style.display="none";
-Dark_mode.style.display="inline-block"
-cntn_container.style.backgroundColor="#111828"
+Normal_mode.addEventListener("click", () => {
+  document.body.classList.remove("dark");
+  document.querySelector("body").style.backgroundColor = "#111828";
+  Normal_mode.style.display = "none";
+  Dark_mode.style.display = "inline-block";
+  cntn_container.style.backgroundColor = "#111828";
 });
